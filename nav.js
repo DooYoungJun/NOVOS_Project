3362bbc7db1d8c4ce67457902fa62a5588501e2f
DooYoungJun <141799483+DooYoungJun@users.noreply.github.com>
2024-06-12 14:31:21 +0900

    Add files via upload

--- nav.js
+++ nav.js
@@ -1,28 +1,10 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var navbarToggle = document.getElementById('navbar-toggle');
-    var menu = document.querySelector('.navbar-menu');
-    var navbar = document.getElementById('navbar');
-
-    navbarToggle.addEventListener('click', function() {
     document.getElementById('navbar-toggle').addEventListener('click', function() {
         var menu = document.querySelector('.navbar-menu');
         menu.classList.toggle('active');
-        navbarToggle.classList.toggle('change');
+        var toggle = document.getElementById('navbar-toggle');
+        toggle.classList.toggle('change');
         var logo = document.querySelector('.navbar-logo');
         logo.classList.toggle('hidden');
-    });
-
-    // 현재 페이지 URL 가져오기
-    var currentPage = window.location.pathname.split("/").pop();
-
-    // 모든 네비게이션 항목 가져오기
-    var navItems = document.querySelectorAll('.navbar-item');
-
-    // 현재 페이지와 일치하는 항목에 active 클래스 추가
-        navItems.forEach(function(item) {
-            if (item.getAttribute('data-link') === currentPage) {
-                item.classList.add('active');
-            }
-        });
     });
 });
--- picture_effect.js
+++ picture_effect.js
@@ -4,6 +4,9 @@ document.addEventListener('DOMContentLoaded', function () {
             if (entry.isIntersecting) {
                 entry.target.classList.add('reveal');
                 observer.unobserve(entry.target); // 요소가 나타나면 관찰 중지
+
+                // 한 번만 효과를 주고 이벤트 리스너를 제거
+                window.removeEventListener('scroll', scrollHandler);
             }
         });
     });
@@ -11,17 +14,18 @@ document.addEventListener('DOMContentLoaded', function () {
     const target = document.querySelector('.color-box');
     observer.observe(target);
 
+    // 스크롤에 따라 이미지의 투명도를 조절하는 이벤트 리스너
+    function scrollHandler() {
+        const scrollY = window.scrollY || window.pageYOffset;
+        const opacity = Math.min(scrollY / 1000, 1); // 최대 투명도를 1로 유지
+        setOpacity(opacity);
+    }
+
     // 이미지 투명도를 조절하는 함수
     function setOpacity(opacity) {
         target.style.opacity = opacity;
     }
 
-    // 스크롤에 따라 이미지의 투명도를 조절
-    window.addEventListener('scroll', function () {
-        // 윈도우의 스크롤 위치를 가져옴
-        const scrollY = window.scrollY || window.pageYOffset;
-        // 이미지의 투명도를 스크롤 위치에 따라 조절 (0에서 1 사이)
-        const opacity = Math.min(scrollY / 1000, 1); // 스크롤 위치에 따라 투명도 조절
-        setOpacity(opacity);
-    });
+    // 스크롤 이벤트 리스너 등록
+    window.addEventListener('scroll', scrollHandler);
 });
