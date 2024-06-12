87d570c2cd11114cb35d9bacbb40cd4447e79a72
Aizergling <zx89321@gmail.com>
2024-06-12 11:48:03 +0900

    nav

--- nav.js
+++ nav.js
@@ -12,15 +12,6 @@ document.addEventListener('DOMContentLoaded', function() {
         logo.classList.toggle('hidden');
     });
 
-    // 스크롤 시 따라오는 네비게이션 바(왜인지 안됨)
-    window.addEventListener('scroll', function() {
-        if (window.scrollY > 0) {
-            navbar.classList.add('scrolled');
-        } else {
-            navbar.classList.remove('scrolled');
-        }
-    });
-
     // 현재 페이지 URL 가져오기
     var currentPage = window.location.pathname.split("/").pop();
 
@@ -28,10 +19,10 @@ document.addEventListener('DOMContentLoaded', function() {
     var navItems = document.querySelectorAll('.navbar-item');
 
     // 현재 페이지와 일치하는 항목에 active 클래스 추가
-    navItems.forEach(function(item) {
-        if (item.getAttribute('data-link') === currentPage) {
-            item.classList.add('active');
-        }
+        navItems.forEach(function(item) {
+            if (item.getAttribute('data-link') === currentPage) {
+                item.classList.add('active');
+            }
         });
     });
 });
